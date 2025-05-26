e465d2ef1ac41f57f5289da35e068c959a7471da
Ilgaz <ilgazcany@gmail.com>
2025-05-26 22:02:21 +0100

    test upd

--- python test/api_tester.py
+++ python test/api_tester.py
@@ -6,3 +6,4 @@ genai.configure(api_key="AIzaSyCWV75v_5yFpqjTy8WABGFwAgxmg3juSq4")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works in a few words")
 print(response.text)
+.
