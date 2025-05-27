2848583b9d0f63d5f72a0edd6e53bb8b78172c03
Ilgaz <ilgazcany@gmail.com>
2025-05-27 13:59:58 +0100

    removed api leak

--- python test/api_tester.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-# Configure the API key
-genai.configure(api_key="AIzaSyCWV75v_5yFpqjTy8WABGFwAgxmg3juSq4")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works in a few words")
-print(response.text)
