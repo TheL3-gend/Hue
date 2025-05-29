61a6bf02abb36bb0c5a6eabc2b2348bcdba0f74a
Ilgaz <ilgazcany@gmail.com>
2025-05-29 15:45:59 +0100

    tailwindcss sorted out

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
   resolve: {
     alias: {
       '@components': '/src/components',
